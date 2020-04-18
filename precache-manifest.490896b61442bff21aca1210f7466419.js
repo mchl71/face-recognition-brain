4402b6d1fdc380fb99586ade03ce7f05ccf8d5bd
Michel Böker <michel-boeker@gmx.de>
2020-04-18 12:23:21 +0200

    Updates

---
+++ precache-manifest.490896b61442bff21aca1210f7466419.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f2eb1c3fd08f6490146b81d82703a3c",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "ab8918055e880fc3852f",
+    "url": "/face-recognition-brain/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "85f8050ea06ae4114363",
+    "url": "/face-recognition-brain/static/css/main.b000b214.chunk.css"
+  },
+  {
+    "revision": "ab8918055e880fc3852f",
+    "url": "/face-recognition-brain/static/js/2.a984e7f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-recognition-brain/static/js/2.a984e7f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85f8050ea06ae4114363",
+    "url": "/face-recognition-brain/static/js/main.07b93f36.chunk.js"
+  },
+  {
+    "revision": "7004dc07691cc6705258",
+    "url": "/face-recognition-brain/static/js/runtime-main.fbadeeaf.js"
+  }
+]);
